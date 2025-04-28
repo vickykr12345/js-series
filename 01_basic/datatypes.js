96cca0cdb1ac7fde645fc7f0b04591e7a88edfa7
vickykr12345 <57521134+vickykr12345@users.noreply.github.com>
2025-04-28 07:11:40 +0000

    data types

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all code as a newer version through the js engine
+
+// alert("hi")
+// this alert is not use in nodejs, because this all the js engine are in web browser and here we are not using any browser. this will work in web browser.
+
+console.log(3*3)
+console.log("ram")
+
+let name = "vicky"      // this is string data types
+let age = 26            // this is number data types
+let is_logged_in = true     // this is boolean data types
+
+/*
+data types:
+--------------------
+number => range 2 to the power of 53
+bigInt => 
+*/
